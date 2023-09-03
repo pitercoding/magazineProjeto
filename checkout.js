d8937d308f6dbf08d90acb4f799bd9bb4189a579
Piter Gomes <piterg.bio@gmail.com>
2023-09-03 21:08:52 +0200

    primeiro commit do intensivao

---
+++ checkout.js
@@ -0,0 +1,33 @@
+import { desenharProdutoSimples, lerLocalStorage, apagarDoLocalStorage, salvarLocalStorage } from "./src/utilidades";
+
+function desenharProdutosCheckout () {
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage('carrinho') ?? {};
+    for ( const idProduto in idsProdutoCarrinhoComQuantidade) {
+       desenharProdutoSimples(idProduto, "container-produtos-chackout", idsProdutoCarrinhoComQuantidade[idProduto]);
+        
+    }
+}
+
+function finalizarCompra(evento) {
+    evento.preventDefault();
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage('carrinho') ?? {};
+    if(Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+        return;
+    }
+    const dataAtual = new Date();
+    const pedidoFeito = {
+        dataPedido: dataAtual,
+        pedido: idsProdutoCarrinhoComQuantidade
+    }
+    const historicoDePedidos = lerLocalStorage('historico') ?? [];
+    const historicoDePedidosAtualizado = [pedidoFeito, ...historicoDePedidos];
+
+    salvarLocalStorage('historico', historicoDePedidosAtualizado);
+    apagarDoLocalStorage('carrinho');
+
+    window.location.href = './pedidos.html';
+}
+
+desenharProdutosCheckout ();
+
+document.addEventListener('submit', (evt) => finalizarCompra(evt));
